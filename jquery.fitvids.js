a9c5b4a4fed366d61f79ce4218a6533dd743de82
beezee <brian.zeligson@gmail.com>
2013-02-24 14:37:25 -0500

    Dont add ids to the videos
    It's not being used anywhere and it is done in a way that can result in dupes

--- jquery.fitvids.js
+++ jquery.fitvids.js
@@ -69,10 +69,6 @@
         var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
             width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
             aspectRatio = height / width;
-        if(!$this.attr('id')){
-          var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-          $this.attr('id', videoID);
-        }
         $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
         $this.removeAttr('height').removeAttr('width');
       });
